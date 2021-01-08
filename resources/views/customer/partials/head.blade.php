83d333f02d0168e0c60d3c5bde189a61efd352f4
tranhoang-c52 <69331444+tranhoang-c52@users.noreply.github.com>
2021-01-08 20:16:23 +0700

    xoa fb page

--- resources/views/customer/partials/head.blade.php
+++ resources/views/customer/partials/head.blade.php
@@ -7,6 +7,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="google-site-verification" content="FmPO275g37H5kixUCgMLJmeKmuYpk-ZX5SXaa6iFhGU" />
     @include('seo_manager.seo_frontend_component')
-    <link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700' rel='preload' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700&display=swap' rel='preload' type='text/css'>
     @include('customer.partials.css')
 </head>
