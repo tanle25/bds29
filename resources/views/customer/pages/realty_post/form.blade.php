8f1fc37526172b694e54d7adccefc8825f196874
tranhoang-c52 <69331444+tranhoang-c52@users.noreply.github.com>
2021-02-03 14:28:19 +0700

    fix giao dien

--- resources/views/customer/pages/realty_post/form.blade.php
+++ resources/views/customer/pages/realty_post/form.blade.php
@@ -121,9 +121,9 @@
     </div>
     <div class="col-md-6">
         <div class="form-group">
-            <label class="control-label">Số phòng<span class="text-danger">*</span></label>
             <div class="row">
                 <div class="col-md-4 mb-3 mb-md-0">
+                    <label class="control-label">Số phòng</label>
                     <input id="number_of_floors" type="number" name="number_of_floors" class="form-control" placeholder="Số tầng" value="{{$realty->number_of_floors ?? old('number_of_floors')}}" >
                     @error('number_of_floors')
                     <div class="text-danger">
@@ -133,6 +133,7 @@
                 </div>
 
                 <div class="col-md-4 has-errors mb-3 mb-md-0">
+                    <label class="control-label">Số phòng tắm</label>
                     <input id="number_of_bath_rooms" type="number" name="number_of_bath_rooms" class="form-control" placeholder="Phòng tắm" value="{{$realty->number_of_bath_rooms ?? old('number_of_bath_rooms')}}" >
                     @error('number_of_floors')
                     <div class="text-danger">
@@ -142,7 +143,8 @@
                 </div>
                 <div class="col-md-4 mb-3 mb-md-0">
                     <div class="form-group">
-                      <input type="number" name="number_of_bed_rooms" id="" class="form-control" placeholder="Phòng ngủ" aria-describedby="" value="{{$realty->number_of_bed_rooms ?? old('number_of_bed_rooms')}}">
+                        <label class="control-label">Số phòng ngủ</label>
+                        <input type="number" name="number_of_bed_rooms" id="" class="form-control" placeholder="Phòng ngủ" aria-describedby="" value="{{$realty->number_of_bed_rooms ?? old('number_of_bed_rooms')}}">
                     </div>
                     @error('number_of_bed_rooms')
                     <div class="text-danger">
@@ -157,9 +159,9 @@
 <div class="row">
     <div class="col-md-6">
         <div class="form-group">
-            <label class="control-label">Giá đề nghị <span class="text-danger">*</span></label>
             <div class="row">
                 <div class="col-md-6 col-sm-4 price-fav mb-3 mb-md-0">
+                    <label class="control-label">Giá đề nghị <span class="text-danger">*</span></label>
                     <input id="price" type="number" name="price" class="form-control" placeholder="Giá đề nghị" value="{{$realty_post->price ?? old('price')}}" onkeyup="onChangeInput(event);">
                     @error('price')
                     <div class="text-danger">
@@ -171,6 +173,7 @@
                 <div class="col-sm-6 col-rate mb-3 mb-md-0">
                     <div class="row">
                         <div class="">
+                            <label class="control-label">Đơn vị <span class="text-danger">*</span></label>
                             <select class="form-control" id="price_type" name="price_type">
                                 @foreach (config('constant.price_type') as $index => $item)
                                     <option
