4b338071428cd9ebeb7ed84ad25c37d93a0f1d52
le tan <tanltps04690@gmail.com>
2022-08-17 14:28:24 +0700

    update

--- resources/views/customer/partials/css.blade.php
+++ resources/views/customer/partials/css.blade.php
@@ -2,8 +2,9 @@
 
 {{-- <link rel="stylesheet" href="{{asset('plugins/fontawesome-pro/css/all.css')}}"> --}}
 {{-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet"> --}}
-<link rel="preload" href="{{asset('css/main.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<noscript><link rel="stylesheet" href="{{asset('css/main.css')}}"></noscript>
+<link rel="stylesheet" href="{{asset('css/main.css')}}">
+{{-- <link rel="preload" href="{{asset('css/main.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript><link rel="stylesheet" href="{{asset('css/main.css')}}"></noscript> --}}
 <link rel="preload" href="{{asset('css/phone-ring.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 <noscript><link rel="stylesheet" href="{{asset('css/phone-ring.css')}}"></noscript>
 <link rel="preload" href="{{asset('css/updatebdsv1.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
