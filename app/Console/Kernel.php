a76886ad9708e9c326e459ab92a6f85ece2eef91
le tan <tanltps04690@gmail.com>
2022-08-27 08:41:29 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('sitemap:create')->dailyAt('00:00');
+        $schedule->command('sitemap:create')->everyMinute();
     }
 
     /**
