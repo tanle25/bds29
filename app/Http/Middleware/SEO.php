b11e6ae5bec1b4a3ad13ae115fef656c6e08160f
le tan <tanltps04690@gmail.com>
2022-09-06 14:54:17 +0700

    update

--- app/Http/Middleware/SEO.php
+++ app/Http/Middleware/SEO.php
@@ -19,7 +19,7 @@ class SEO
     {
         if(Str::endsWith($request->getRequestUri(),'?')){
             $url = Str::replaceLast('?','',$request->requestUri);
-            dd($request);
+            // dd($request);
             return redirect($request->url());
         }
         return $next($request);
