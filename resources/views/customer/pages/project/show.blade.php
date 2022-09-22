9151708953fa7d4fedee20dc0c8145947c085ebc
le tan <tanltps04690@gmail.com>
2022-09-22 09:26:05 +0700

    update

--- resources/views/customer/pages/project/show.blade.php
+++ resources/views/customer/pages/project/show.blade.php
@@ -339,6 +339,20 @@
             autoplayTimeout: 3000,
             autoplaySpeed: 1200,
             smartSpeed: 1200,
+            responsive : {
+                // breakpoint from 0 up
+                0 : {
+                    items: 1
+                },
+                // breakpoint from 480 up
+                480 : {
+                    items: 2
+                },
+                // breakpoint from 768 up
+                768 : {
+                    items: 3,
+                }
+            }
         });
     </script>
 @endsection
